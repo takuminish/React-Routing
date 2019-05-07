e101ac100b3e0164efbe9b2272791488a89eed6e
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-05-07 20:58:29 +0900

    遷移するためのリンクを設定

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 const App = () => (
     <BrowserRouter>
         <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/hobby">Hobby</Link></li>
+                <li><Link to="/skill">Skill</Link></li>
+            </ul>
             <Route exact path="/" component={Home} />
             <Route path="/hobby" component={Hobby} />
             <Route path="/skill" component={Skill} />
