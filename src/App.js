07448ba47bf8e07f563273f0817c3ae5442b05d7
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-05-07 17:42:52 +0900

    React-routerをimport

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 class App extends Component {
+    
     render() {
         return (
 	    <h1>app</h1>
