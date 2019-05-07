98ab52788d01066fe2833ec60360e3d68b71cead
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-05-07 17:41:27 +0900

    render部分を記述

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
-	return (
-		<h1>app</h1>
+        return (
+	    <h1>app</h1>
 	);
     }
 }
 
 class Home extends Component {
+    render() {
+        return(
+	    <h1>Home</h1>
+	);
+    }
 
 }
 
 class Skill extends Component {
+    render() {
+        return(
+            <h1>Skill</h1>
+	);
+    }
 
 }
 
 class Hobby extends Component {
-
+    render() {
+        return(
+            <h1>Hobby</h1>
+	);
+    }
 }
 
 export default App;
